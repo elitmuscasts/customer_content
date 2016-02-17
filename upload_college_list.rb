3cfcf398683b3517b6c26f78529b6998e7b42906
bitsapien <bitsapien@gmail.com>
2016-02-17 13:21:20 +0530

    implemented search

--- upload_college_list.rb
+++ upload_college_list.rb
@@ -2,7 +2,7 @@
 # UPLOAD COLLEGE LIST AUTOMATOR
 # Usage :
 #   ruby upload_college_list.rb <input_file_path.csv> <output_file_path.html>
-#   <input_file_path.csv> # input file must be a CSV with first column with heading 'Serial #' and 'College Name'
+#   <input_file_path.csv> # input file must be a CSV with first column with heading 'College Name'
 #   <output_file_path.html> # must be an html file
 require 'csv'
 
@@ -10,7 +10,6 @@ require 'csv'
 	def convert_file(csv_file, to_file)
 
 		csv_rows= CSV.read(csv_file,:headers => true , :encoding => 'ISO-8859-1')
-        
 
 
 		File.open(to_file,'w') do |f|
@@ -29,15 +28,28 @@ require 'csv'
                             <div class="row">
                                 <div class="col-md-offset-1"/>
                                 <div class="col-md-10">
+                                    <br/>
+                                    <div id="custom-search-input">
+                                       <div class="input-group col-md-12">
+                                           <input type="text" id="search-input" class="form-control input-md" placeholder="search college name..." />
+                                               <span class="input-group-btn">
+                                                   <button class="btn btn-info btn-md" type="button">
+                                                      <i class="glyphicon glyphicon-search"></i>
+                                                    </button>
+                                                </span>
+                                        </div>
+                                    </div>
+                                    <br/>
                                     <div class="table-responsive">
-                                        <table class="table table-striped table-hover table-condensed table-bordered">')
+                                        <table id="college-list" class="table table-striped table-hover table-condensed table-bordered">')
           
           # adding the colleges one by one
+      idx = 0
 		  csv_rows.each do |row|
-            number = row['Serial #']
             college_name = row['College Name']
-            string = "<tr><td>#{number}</td><td>#{college_name}</td></tr>\n"
+            string = "<tr id='s_no_#{idx}'><td>#{idx+1}</td><td>#{college_name}</td></tr>\n"
             f.write (string)
+            idx = idx + 1
 		  end
           
           # adding the footer
@@ -55,7 +67,41 @@ require 'csv'
             </div>
         </div>
         </body>
-')
+    <script src="https://code.jquery.com/jquery-2.2.0.min.js"></script>
+        <script>
+        // search
+        var college_list = {};
+        $("#college-list tr").each(function(row, tr){
+            college_list[row] = {
+            "serial"        : $(tr).find("td:eq(0)").text()  // Serial No.
+            , "college_name": $(tr).find("td:eq(1)").text()  // College Name
+            }
+        });
+
+        console.log(college_list);
+
+        $(document).ready(function(){
+            $("#search-input").keyup(function(){
+                 // Retrieve the input field text and reset the count to zero
+                 var filter = $(this).val();
+                 for (var key in college_list) {
+                     if (college_list.hasOwnProperty(key)) {
+                          var val = college_list[key];
+                          if ((val.college_name.search(new RegExp(filter, "i")) < 0)){
+                               $("#s_no_"+(val.serial-1)).fadeOut();
+
+
+                           }
+                           else{
+                                $("#s_no_"+(val.serial-1)).show();
+                           }
+
+            }
+        }
+    })
+});
+    </script>
+</html>')
         end
 	end
 
